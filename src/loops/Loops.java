27a4cd4e8ccb3adc9ebca92ad5692e8a647a5880
Indrajith2502 <144647834+Indrajith2502@users.noreply.github.com>
2023-09-14 10:26:08 +0530

    first commit

---
+++ src/loops/Loops.java
@@ -0,0 +1,27 @@
+package loops;
+
+public class Loops {
+
+	public static void main(String[] args) {
+		
+	
+	 String str ="*";
+	 int i= 0;
+	 int j =1;
+	 int k = 5;
+	 while(j<=k) {
+		 while(i<=j-1) {
+		 System.out.print(str);
+		
+		 i++;
+		 }
+		 j++;
+		 i=0;
+		 System.out.println(" ");
+		 
+		 
+	 }
+	 }
+}
+
+
